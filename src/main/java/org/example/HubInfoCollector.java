cff854c3ae85bafa4f07fe69ca8c20f6e4d2294d
elek9716 <elek97@stud.uni-obuda.hu>
2025-03-09 16:02:12 +0100

    Created HubInfoCollector class and wrote the main class.

--- src/main/java/org/example/HubInfoCollector.java
+++ src/main/java/org/example/HubInfoCollector.java
@@ -1,5 +1,31 @@
 package org.example;
+import com.ibm.cloud.cloudant.v1.Cloudant;
+import com.ibm.cloud.cloudant.v1.model.*;
+
+import java.util.Arrays;
 
 public class HubInfoCollector {
+    private Cloudant service;
+    private String dbName;
+    private String ddocName;
+    private String indexName;
+    public HubInfoCollector()
+    {
+        this.service = Cloudant.newInstance();
+        this.dbName = "airportdb";
+        this.ddocName = "view1";
+        this.indexName = "geo";
 
+    }
+    public SearchResult getClosestHubsDetails(double fromLat, double toLat, double fromLon, double toLon, double userLat, double userLon) {
+        PostSearchOptions searchOptions = new PostSearchOptions.Builder()
+                .db(dbName)
+                .ddoc(ddocName)
+                .index(indexName)
+                .query(String.format("lat:[%s TO %s] AND lon:[%s TO %s]", fromLat, toLat, fromLon, toLon))
+                .sort(Arrays.asList(String.format("<distance,lon,lat,%s,%s,km>", userLat, userLon)))
+                .build();
+        SearchResult result = service.postSearch(searchOptions).execute().getResult();
+        return result;
+    }
 }
--- src/main/java/org/example/TransportApp.java
+++ src/main/java/org/example/TransportApp.java
@@ -12,8 +12,34 @@ import java.util.*;
 public class TransportApp {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        var client = Cloudant.newInstance();
+        System.out.println("Enter the latitude range!");
+        System.out.println("From:");
+        double fromLat = scanner.nextDouble();
+        System.out.println("To:");
+        double toLat = scanner.nextDouble();
+
+        System.out.println("Enter the longitude range!");
+        System.out.println("From:");
+        double fromLon = scanner.nextDouble();
+        System.out.println("To:");
+        double toLon = scanner.nextDouble();
+
+        System.out.println("Enter your location's latitude!");
+        System.out.println("From:");
+        double userLat = scanner.nextDouble();
+        System.out.println("To:");
+        System.out.println("Enter your location's longitude!");
+        double userLon = scanner.nextDouble();
+
+        var hubInfoCollector = new HubInfoCollector();
+        var response = hubInfoCollector.getClosestHubsDetails(fromLat,toLat,fromLon,toLon,userLat,userLon);
+
+        System.out.println(response);
+
+
+       // var client = Cloudant.newInstance();
        /* System.out.println(client.getAllDbs());
         System.out.println(client.getServiceUrl());
         var serverInfo = client.getServerInformation().execute().getResult();
@@ -24,12 +50,12 @@ public class TransportApp {
 
 
 
-        PostSearchOptions searchOptions = new PostSearchOptions.Builder().db("airportdb").ddoc("view1").index("geo")
-                .query("lat:[47.3 TO 47.8] AND lon:[19.0 TO 19.5]").sort(Arrays.asList("<distance,lon,lat,-47.3,40.7,km>")).build();
+        /*PostSearchOptions searchOptions = new PostSearchOptions.Builder().db("airportdb").ddoc("view1").index("geo")
+                .query("lat:[47.3 TO 47.8] AND lon:[19.0 TO 19.5]").sort(Arrays.asList("<distance,lon,lat,-47.3,41.7,km>")).build();
        ;
         SearchResult responsee = client.postSearch(searchOptions).execute().getResult();
 
-        System.out.println(responsee);
+        System.out.println(responsee);*/
 
 
 
