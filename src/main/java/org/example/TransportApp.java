7a6fa5b694412d4cffd1311a4fd0f3758af5ed0b
elek9716 <elek97@stud.uni-obuda.hu>
2025-03-09 16:07:35 +0100

    Removed code used for testing

--- src/main/java/org/example/TransportApp.java
+++ src/main/java/org/example/TransportApp.java
@@ -39,23 +39,7 @@ public class TransportApp {
         System.out.println(response);
 
 
-       // var client = Cloudant.newInstance();
-       /* System.out.println(client.getAllDbs());
-        System.out.println(client.getServiceUrl());
-        var serverInfo = client.getServerInformation().execute().getResult();
-        System.out.println(serverInfo);
-        System.out.println("---------");
-        String dbName = "airportdb";*/
 
-
-
-
-        /*PostSearchOptions searchOptions = new PostSearchOptions.Builder().db("airportdb").ddoc("view1").index("geo")
-                .query("lat:[47.3 TO 47.8] AND lon:[19.0 TO 19.5]").sort(Arrays.asList("<distance,lon,lat,-47.3,41.7,km>")).build();
-       ;
-        SearchResult responsee = client.postSearch(searchOptions).execute().getResult();
-
-        System.out.println(responsee);*/
 
 
 
